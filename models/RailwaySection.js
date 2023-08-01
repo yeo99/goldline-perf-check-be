03309d1077e1a8796b6757a1fbeb4d97b2bc3ad8
yeo99 <qw6375@gmail.com>
2023-08-02 02:22:50 +0900

    [DEL] RailwaySection controller 주석 제거

--- models/RailwaySection.js
+++ models/RailwaySection.js
@@ -1,27 +1,3 @@
-// const { Model, DataTypes } = require('sequelize');
-// const db = require('../models/index')
-// const sequelize = db.sequelize
-
-// class RailwaySection extends Model {}
-
-// RailwaySection.init({
-//     id: {
-//         type: DataTypes.INTEGER.UNSIGNED,
-//         allowNull: false,
-//         primaryKey: true,
-//     },
-//     section_name: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     }
-// }, {
-//     sequelize,
-//     modelName: 'RailwaySection',
-//     tableName: 'railway_sections',
-//     timestamps: false,
-// });
-
-// module.exports = RailwaySection;
 'use strict'
 
 module.exports = (sequelize, DataTypes) => {
