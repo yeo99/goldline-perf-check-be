c4e4ee234ecd5c955848e465aa4b8f7c0b870c18
yeo99 <qw6375@gmail.com>
2023-08-07 15:10:11 +0900

    [ADD] 대분류 시설 컨트롤러 추가

--- routes/router.js
+++ routes/router.js
@@ -5,8 +5,16 @@ const router = express.Router();
 const { getAllRailwaySection } = require('../controllers/RailwaySection')
 // 역 코드 controller
 const { getAllRailwayStation } = require('../controllers/RailwayStation')
+// 대분류 read controller
+const { getAllFacilitiesHighClass } = require('../controllers/FacilitiesHighClass')
+// 중분류 검색 controller
+const { getFacilitiesMiddleClass } = require('../controllers/FacilitiesMiddleClass')
+
 
 router.get('/railway/sections', getAllRailwaySection)
 router.get('/railway/stations', getAllRailwayStation)
+
+router.get('/facilities/high-class/', getAllFacilitiesHighClass)
+router.get('/facilities/middle-class/:upper_category', getFacilitiesMiddleClass)
 
 module.exports = router
