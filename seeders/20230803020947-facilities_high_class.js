956cbde1d12c94da7ca3e484acd191d26562e304
yeo99 <qw6375@gmail.com>
2023-08-30 14:43:32 +0900

    [ADD] 누락된 대분류 카테고리 추가

--- seeders/20230803020947-facilities_high_class.js
+++ seeders/20230803020947-facilities_high_class.js
@@ -8,7 +8,8 @@ module.exports = {
       { id: 'B000', facility_code: '궤도시설' },
       { id: 'C000', facility_code: '건축물' },
       { id: 'D000', facility_code: '전철전력설비' },
-      { id: 'E000', facility_code: '신호제어설비' }
+      { id: 'E000', facility_code: '신호제어설비' },
+      { id: 'F000', facility_code: '정보통신설비' }
     ]
 
     await queryInterface.bulkInsert('facilities_high_class', facilitiesHighClass, {})
